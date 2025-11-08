a654271a512d383ea65e96cfd4ebc873429c4acf
poyrazavsever <poyrazavsever@hotmail.com>
2025-11-08 13:23:57 +0300

    feat: created docs layout with navbar and footer

---
+++ app/components/layout/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='container max-w-6xl mx-auto px-6 sm:px-0'>
+
+        nav
+      
+    </nav>
+  )
+}
+
+export default Navbar
